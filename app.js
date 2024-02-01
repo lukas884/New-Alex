9bbb643f854b56fbf407449a92eb482b7b19e4db
lukas884 <157029390+lukas884@users.noreply.github.com>
2024-02-01 14:40:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
 })
+
+
+function openMenu(evt, menuName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("menu");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+       tablinks[i].className = tablinks[i].className.replace(" w3-light-green", "");
+    }
+    document.getElementById(menuName).style.display = "block";
+    evt.currentTarget.firstElementChild.className += " w3-light-green";
+  }
+  document.getElementById("myLink").click();
